140abe0c9b72f5471115b97afe8d636a506b6652
Kenjali <36539309+Evgeny-Zhukov@users.noreply.github.com>
2024-04-03 15:31:11 +0300

    Обновил пути к картинкам

--- ActiveCampWPF/MainWindow.xaml.cs
+++ ActiveCampWPF/MainWindow.xaml.cs
@@ -1,14 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -16,13 +14,82 @@ using System.Windows.Shapes;
 namespace ActiveCampWPF
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            MenuBackground.Visibility = Visibility.Visible;
+            MenuBackground.Focusable = true;
+
+            MenuPanel.Visibility = Visibility.Visible;
+            MenuPanel.Focusable = true;
+        }
+
+        private void AccountButton_Click(object sender, RoutedEventArgs e)
+        {
+            CloseMenu();
+            //Treatment of account button.
+        }
+
+        private void NewsButton_Click(object sender, RoutedEventArgs e)
+        {
+            CloseMenu();
+            //Treatment of News button.
+        }
+        private void HikingButton_Click(object sender, RoutedEventArgs e)
+        {
+            CloseMenu();
+            //Treatment of Hiking button.
+        }
+        private void EquipmentButton_Click(object sender, RoutedEventArgs e)
+        {
+            CloseMenu();
+            //Treatment of Equipment button.
+        }
+
+        private void SettingButton_Click(object sender, RoutedEventArgs e)
+        {
+            CloseMenu();
+            //Treatment of Setting button.
+        }
+
+        private void MenuBackgroundButton_Click(object sender, RoutedEventArgs e)
+        {
+            CloseMenu();
+            //Treatment of BackGaround button.
+        }
+
+        private void CloseMenu()
+        {
+            MenuBackground.Focusable = false;
+            MenuBackground.Visibility = Visibility.Hidden;
+
+            DoubleAnimation MenuCloseAnimation = new DoubleAnimation
+            {
+                From = 320,
+                To = 0,
+                Duration = new Duration(TimeSpan.Parse("0:0:0.5"))
+            };
+            MenuPanel.BeginAnimation(Grid.WidthProperty, MenuCloseAnimation);
+            MenuPanel.Focusable = false;
+            MenuPanel.Visibility = Visibility.Hidden;
+        }
+
+        private void ClientStatus_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void NewsList_SelectionChanged()
+        {
+
         }
     }
 }
