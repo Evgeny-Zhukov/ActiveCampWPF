b60cd1130ffb3aff603b6d52b12638d76268988f
GituserRU <email02011rid@gmail.com>
2024-04-06 21:40:25 +0300

    Пробное обновление

--- ActiveCampWPF/MainWindow.xaml.cs
+++ ActiveCampWPF/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -19,6 +20,75 @@ namespace ActiveCampWPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            MenuBackground.Visibility = Visibility.Visible;
+            MenuBackground.Focusable = true;
+
+            MenuPanel.Visibility = Visibility.Visible;
+            MenuPanel.Focusable = true;
+        }
+
+        private void AccountButton_Click(object sender, RoutedEventArgs e)
+        {
+            CloseMenu();
+            //Treatment of account button.
+        }
+
+        private void NewsButton_Click(object sender, RoutedEventArgs e)
+        {
+            CloseMenu();
+            //Treatment of News button.
+        }
+        private void HikingButton_Click(object sender, RoutedEventArgs e)
+        {
+            CloseMenu();
+            //Treatment of Hiking button.
+        }
+        private void EquipmentButton_Click(object sender, RoutedEventArgs e)
+        {
+            CloseMenu();
+            //Treatment of Equipment button.
+        }
+
+        private void SettingButton_Click(object sender, RoutedEventArgs e)
+        {
+            CloseMenu();
+            //Treatment of Setting button.
+        }
+
+        private void MenuBackgroundButton_Click(object sender, RoutedEventArgs e)
+        {
+            CloseMenu();
+            //Treatment of BackGaround button.
+        }
+
+        private void CloseMenu()
+        {
+            MenuBackground.Focusable = false;
+            MenuBackground.Visibility = Visibility.Hidden;
+
+            DoubleAnimation MenuCloseAnimation = new DoubleAnimation
+            {
+                From = 320,
+                To = 0,
+                Duration = new Duration(TimeSpan.Parse("0:0:0.5"))
+            };
+            MenuPanel.BeginAnimation(Grid.WidthProperty, MenuCloseAnimation);
+            MenuPanel.Focusable = false;
+            MenuPanel.Visibility = Visibility.Hidden;
+        }
+
+        private void ClientStatus_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void NewsList_SelectionChanged()
+        {
+
         }
     }
 }
