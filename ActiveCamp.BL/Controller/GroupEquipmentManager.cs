253cd7baa30d3c04f6c977a7bd2653ff29e7f0d8
Kenjali <36539309+Evgeny-Zhukov@users.noreply.github.com>
2024-06-17 15:07:20 +0300

    GroupEquipment

---
+++ ActiveCamp.BL/Controller/GroupEquipmentManager.cs
@@ -0,0 +1,160 @@
+﻿using ActiveCamp.BL.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ActiveCamp.BL.Controller
+{
+    internal class GroupEquipmentManager
+    {
+        private ActiveCampDbContext dbContext;
+        private SqlConnection _connection;
+        public GroupEquipmentManager()
+        {
+            dbContext = new ActiveCampDbContext();
+            _connection = dbContext.GetSqlConnection();
+        }
+        public bool AddGroupDish(List<GroupEquipment> groupEquipment)
+        {
+
+            using (_connection)
+            {
+                _connection.Open();
+                using (SqlCommand command = new SqlCommand("AddGroupEquipment", _connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    SqlParameter successParameter = new SqlParameter("@success", SqlDbType.Bit);
+                    successParameter.Direction = ParameterDirection.Output;
+                    command.Parameters.Add(successParameter);
+
+                    SqlParameter GroupEquipmentParameter = new SqlParameter("@GroupEquipmentID", SqlDbType.Int);
+                    GroupEquipmentParameter.Direction = ParameterDirection.Output;
+                    command.Parameters.Add(GroupEquipmentParameter);
+
+                    try
+                    {
+                        foreach (var item in groupEquipment)
+                        {
+                            command.Parameters.Clear();
+
+                            command.Parameters.Add(new SqlParameter("@GroupID", item.GroupID));
+                            command.Parameters.Add(new SqlParameter("@UserID", item.UserID));
+                            command.Parameters.Add(new SqlParameter("@EquipmentName", item.EquipmentName));
+                            command.Parameters.Add(new SqlParameter("@Weigth", item.Weigth));
+                            command.Parameters.Add(new SqlParameter("@Count", item.Count));
+
+                            command.Parameters.Add(successParameter);
+                            command.Parameters.Add(GroupEquipmentParameter);
+
+                            command.ExecuteNonQuery();
+
+                            item.GroupEquipmentID = (int)GroupEquipmentParameter.Value;
+                        }
+
+                        bool success = (bool)successParameter.Value;
+                        return success;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error: " + ex.Message);
+                        return false;
+                    }
+                }
+            }
+        }
+
+        public List<GroupEquipment> GetGroupEquipmentById(int groupID)
+        {
+            List<GroupEquipment> routes = new List<GroupEquipment>();
+
+            using (_connection)
+            {
+                string query = "SELECT * FROM GroupEquipment WHERE GroupID = @GroupID";
+                SqlCommand command = new SqlCommand(query, _connection);
+                command.Parameters.AddWithValue("@GroupID", groupID);
+
+                try
+                {
+                    _connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            GroupEquipment route = new GroupEquipment
+                            {
+                                GroupEquipmentID = Convert.ToInt32(reader["GroupEquipmentID"]),
+                                GroupID = Convert.ToInt32(reader["GroupID"]),
+                                UserID = Convert.ToInt32(reader["UserID"]),
+                                EquipmentName = reader["EquipmentName"].ToString(),
+                                Weigth = Convert.ToInt32(reader["Weigth"]),
+                                Count = Convert.ToInt32(reader["Count"]),
+                            };
+                            routes.Add(route);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+            return routes;
+        }
+
+        public bool UpdateGroupEquipment(GroupEquipment groupEquipment)
+        {
+            using (_connection)
+            {
+                using (SqlCommand command = new SqlCommand("UpdateGroupEquipment", _connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    command.Parameters.Add(new SqlParameter("@GroupEquipmentID", groupEquipment.GroupEquipmentID));
+                    command.Parameters.Add(new SqlParameter("@GroupID", groupEquipment.GroupID));
+                    command.Parameters.Add(new SqlParameter("@UserID", groupEquipment.UserID));
+                    command.Parameters.Add(new SqlParameter("@EquipmentName", groupEquipment.EquipmentName));
+                    command.Parameters.Add(new SqlParameter("@Weigth", groupEquipment.Weigth));
+                    command.Parameters.Add(new SqlParameter("@Count", groupEquipment.Count));
+
+                    SqlParameter successParameter = new SqlParameter("@success", SqlDbType.Bit);
+                    successParameter.Direction = ParameterDirection.Output;
+                    command.Parameters.Add(successParameter);
+
+                    try
+                    {
+                        _connection.Open();
+                        command.ExecuteNonQuery();
+
+                        bool success = (bool)successParameter.Value;
+                        return success;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error: " + ex.Message);
+                        return false;
+                    }
+                }
+            }
+        }
+
+
+        public bool DeleteGroupDish(int groupEquipmentID)
+        {
+            using (_connection)
+            {
+                SqlCommand command = new SqlCommand("DeleateGroupEquipmentById", _connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@GroupEquipmentID", groupEquipmentID);
+                SqlParameter successParameter = new SqlParameter("@success", SqlDbType.Bit);
+                successParameter.Direction = ParameterDirection.Output;
+                command.Parameters.Add(successParameter);
+                _connection.Open();
+                command.ExecuteNonQuery();
+                bool success = (bool)successParameter.Value;
+                return success;
+            }
+        }
+    }
+}
---
+++ ActiveCamp.BL/Model/GroupEquipment.cs
@@ -0,0 +1,141 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ActiveCamp.BL.Model
+{
+    public class GroupEquipment : INotifyPropertyChanged, IEditableObject
+    {
+        private int _groupEquipmentID;
+        private int _groupID;
+        private int _userID;
+        private string _equipmentName;
+        private int _weigth;
+        private int _count;
+
+
+        public int GroupEquipmentID
+        {
+            get { return this._groupEquipmentID; }
+            set
+            {
+                if (value != this._groupEquipmentID)
+                {
+                    this._groupEquipmentID = value;
+                    NotifyPropertyChanged("GroupEquipmentID");
+                }
+            }
+        }
+        public int GroupID
+        {
+            get { return this._groupID; }
+            set
+            {
+                if (value != this._groupID)
+                {
+                    this._groupID = value;
+                    NotifyPropertyChanged("GroupID");
+
+                }
+            }
+        }
+        public int UserID
+        {
+            get { return this._userID; }
+            set
+            {
+                if (value != this._userID)
+                {
+                    this._userID = value;
+                    NotifyPropertyChanged("UserID");
+                }
+            }
+        }
+        public string EquipmentName
+        {
+            get { return this._equipmentName; }
+            set
+            {
+                if (value != this._equipmentName)
+                {
+                    this._equipmentName = value;
+                    NotifyPropertyChanged("EquipmentName");
+                }
+            }
+        }
+        public int Weigth
+        {
+            get { return this._weigth; }
+            set
+            {
+                if (value != this._weigth)
+                {
+                    this._weigth = value;
+                    NotifyPropertyChanged("Weigth");
+                }
+            }
+        }
+        public int Count
+        {
+            get { return this._count; }
+            set
+            {
+                if (value != this._count)
+                {
+                    this._count = value;
+                    NotifyPropertyChanged("Count");
+                }
+            }
+        }
+        public GroupEquipment() { }
+        public GroupEquipment(int groupID, int userID, int weigth, int count = 0, string equipmentName = "")
+        {
+            this._groupID = groupID;
+            this._userID = userID;
+            this._weigth = weigth;
+            this._count = count;
+            this._equipmentName = equipmentName;
+        }
+        private GroupEquipment temp_Record = null;
+        private bool m_Editing = false;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void BeginEdit()
+        {
+            if (m_Editing == false)
+            {
+                temp_Record = this.MemberwiseClone() as GroupEquipment;
+                m_Editing = true;
+            }
+        }
+
+        public void CancelEdit()
+        {
+            if (m_Editing == true)
+            {
+                _groupID = temp_Record.GroupID;
+                _groupEquipmentID = temp_Record.GroupEquipmentID;
+                _userID = temp_Record.UserID;
+                _weigth = temp_Record.Weigth;
+                _count = temp_Record.Count;
+                _equipmentName = temp_Record.EquipmentName;
+
+
+                m_Editing = false;
+            }
+        }
+        public void EndEdit()
+        {
+            if (m_Editing == true)
+            {
+                temp_Record = null;
+                m_Editing = false;
+            }
+        }
+    }
+}
