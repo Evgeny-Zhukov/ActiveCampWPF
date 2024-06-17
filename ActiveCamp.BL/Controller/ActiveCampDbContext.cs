2433723f5fcb81b2696b68182c37502307aaf9cd
Kenjali <36539309+Evgeny-Zhukov@users.noreply.github.com>
2024-06-17 21:09:22 +0300

    #

--- ActiveCamp.BL/Controller/ActiveCampDbContext.cs
+++ ActiveCamp.BL/Controller/ActiveCampDbContext.cs
@@ -9,8 +9,8 @@ namespace ActiveCamp.BL.Model
     // TODO: Можно выгрузить в xml 
     public class ActiveCampDbContext
     {
-        //private readonly string _connectionString = "Server=DESKTOP-VJNL8L9;Database = HikingAppDB;Trusted_Connection=True;MultipleActiveResultSets=True";
-        private readonly string _connectionString = "Server=DESKTOP-MONLUDU;Database = HikingAppDB;Trusted_Connection=True;MultipleActiveResultSets=True";
+        private readonly string _connectionString = "Server=DESKTOP-VJNL8L9;Database = HikingAppDB;Trusted_Connection=True;MultipleActiveResultSets=True";
+        //private readonly string _connectionString = "Server=DESKTOP-MONLUDU;Database = HikingAppDB;Trusted_Connection=True;MultipleActiveResultSets=True";
 
         public ActiveCampDbContext()
         {
